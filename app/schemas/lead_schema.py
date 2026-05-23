0b5d1a1b17fc240143a24762084b4087251e5ccd
francoruiz48 <franco.ruiz@nomadesoft.com.ar>
2026-05-22 21:04:18 -0300

    [ADD] se agrega datos de estados al lead.

--- app/schemas/lead_schema.py
+++ app/schemas/lead_schema.py
@@ -6,6 +6,7 @@ from app.schemas.lead_comment_shema import LeadCommentDetailedResponse
 from app.schemas.lead_field_value_schema import LeadFieldValueCreate, LeadFieldValueDetailedResponse, LeadFieldValueResponse
 from app.schemas.lead_state_schema import LeadStateDetailedResponse, LeadStateResponse
 from app.schemas.tag_schema import TagResponse
+from app.schemas.lead_contact_state_schema import LeadContactStateResponse
 
 
 class LeadBase(BaseModel):
@@ -30,11 +31,14 @@ class LeadResponse(LeadBase, BaseResponse):
     )
     organization_id : int
     current_state_id: int
+    current_state: LeadStateResponse
+    contact_state: LeadContactStateResponse
     tags: List[TagResponse] = Field(default_factory=list)
 
 class LeadLiteResponse(LeadBase, BaseResponse):
     organization_id : int
     current_state_id: int
+
 
 class LeadDetailedResponse(LeadBase, BaseDetailedResponse):
     field_values: List[LeadFieldValueDetailedResponse] = Field(
